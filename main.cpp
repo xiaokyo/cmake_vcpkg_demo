f26871f6882f0074d8c10b81bf4f55e0ceb1be8f
小K <904656778@qq.com>
2023-06-08 22:25:49 +0800

    cmake demo

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <crow.h>
+
+using namespace std;
+
+int main(int argc, const char *argv[])
+{
+    crow::SimpleApp app;
+
+    CROW_ROUTE(app, "/")
+    ([]()
+     { return "Hello world"; });
+
+    CROW_ROUTE(app, "/hello/<int>")
+    ([](int count)
+     {
+    if (count > 100)
+        return crow::response(400);
+    std::ostringstream os;
+    os << count << " bottles of beer!";
+    return crow::response(os.str()); });
+
+    app.port(18080).multithreaded().run();
+    return 0;
+}
